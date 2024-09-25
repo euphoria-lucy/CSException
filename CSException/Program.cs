81925d4ab9167451c65df5f929258f287804751a
Misung Yang <voielactee7@naver.com>
2024-09-25 09:51:10 +0900

    예외처리 try - catch - finally

--- CSException/Program.cs
+++ CSException/Program.cs
@@ -53,9 +53,21 @@ namespace CSException
 
         static void Main(string[] args)
         {
-            Box b1 = new Box(10, 10);
-            Console.WriteLine("Area : {0}", b1.Area);
-            b1.Width = 0;
+            try
+            {
+                Box b1 = new Box(10, 10);
+                Console.WriteLine("Area : {0}", b1.Area);
+                b1.Width = 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("프로그램 종료");
+
+            }
         }
     }
 }
